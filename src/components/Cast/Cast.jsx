7632dfd25c5e55243addc89b122525263d8c8394
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-02-14 14:57:52 +0100

    add basename

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router';
 import { fetchCastInfo } from '../../services/apiService';
 import { BASE_IMG_URL } from '../../services/constans';
 
-import {Container, List} from './Cast.styled'
+import { Container, List } from './Cast.styled';
 
 const Cast = () => {
   const { id } = useParams();
@@ -14,18 +14,15 @@ const Cast = () => {
 
   return (
     <Container>
-      
       {cast.map(actor => (
         <List key={actor.id}>
-          <img src={BASE_IMG_URL + actor.profile_path} alt="" width='150'/>
+          <img src={BASE_IMG_URL + actor.profile_path} alt="" width="150" />
           <p>{actor.name}</p>
           <p>character: {actor.character}</p>
         </List>
-        
       ))}
     </Container>
   );
 };
 
-
-export default Cast
+export default Cast;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,31 +2,27 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { fetchReviewsInfo } from '../../services/apiService';
 
-import {Container} from './Reviews.styled'
+import { Container } from './Reviews.styled';
 
 const Reviews = () => {
   const { id } = useParams();
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetchReviewsInfo(id)
-    .then(setReviews);
+    fetchReviewsInfo(id).then(setReviews);
   }, [id]);
 
   return (
     <Container>
-        {reviews?.length === 0 && (
-            <p>We don't have any reviews for this movie</p>
-        )}
-      
+      {reviews?.length === 0 && <p>We don't have any reviews for this movie</p>}
+
       {reviews.map(review => (
         <li key={review.id}>
-            Author: {review.author}
-            <p>{review.content}</p>
+          Author: {review.author}
+          <p>{review.content}</p>
         </li>
       ))}
     </Container>
   );
 };
 
-
-export default Reviews
+export default Reviews;
